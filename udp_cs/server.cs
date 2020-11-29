1aa9588c0eb14139e965dfb14531eda17b425b10
adegh3ik2nos <adegh3ik2nos@gmail.com>
2020-11-29 15:10:52 +0900

    タイムアウト追加

--- udp_cs/server.cs
+++ udp_cs/server.cs
@@ -1,77 +1,59 @@
 ﻿
 using System;
-using System.Text;
-using System.Runtime.InteropServices;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-namespace test10
+namespace test20
 {
     class Program
     {
-        // Win32 APIであるSetConsoleCtrlHandler関数の宣言
-        public enum CtrlTypes
-        {
-            CTRL_C_EVENT = 0,
-            CTRL_BREAK_EVENT = 1,
-            CTRL_CLOSE_EVENT = 2,
-            CTRL_LOGOFF_EVENT = 5,
-            CTRL_SHUTDOWN_EVENT = 6
-        }
-        delegate bool HandlerRoutine(CtrlTypes CtrlType);
-        [DllImport("Kernel32")]
-        static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
-
-        //サーバ終了通知
-        private static bool _OnKill(CtrlTypes ctrlType)
-        {
-            m_udpClient?.Close();
-
-            return false;
-        }
-
         static void Main()
         {
-            //終了時ハンドル
-            HandlerRoutine killHandler;
-            killHandler = new HandlerRoutine(_OnKill);
-            SetConsoleCtrlHandler(killHandler, true);
-
-
-            //バインドするローカルIPとポート番号
-            string localIp = "127.0.0.1";
-            IPAddress localAddress = IPAddress.Parse(localIp);
-            int localPort = 2002;
-
-            //UdpClientを作成し、ローカルエンドポイントにバインドする
-            IPEndPoint localEP =new IPEndPoint(localAddress, localPort);
-            m_udpClient = new UdpClient(localEP);
-            if(null != m_udpClient)
+            //データを送信するリモートホストとポート番号
+            string remoteHost = "127.0.0.1";
+            IPAddress remoteAddress = IPAddress.Parse(remoteHost);
+            int remotePort = 2002;
+            
+            UdpClient udpClient = new UdpClient();
+            if (null != udpClient)
             {
-                while (true)
+                //ビルドのクエリを投げる
                 {
-                    IPEndPoint remoteEP = null;
-                    byte[] rcvBytes = null;
-                    rcvBytes = m_udpClient?.Receive(ref remoteEP);
-                    string rcvMsg = Encoding.UTF8.GetString(rcvBytes);
-
-                    //受信したデータと送信者の情報を表示する
-                    Console.WriteLine("受信したデータ:{0}", rcvMsg);
-                    Console.WriteLine("送信元アドレス:{0}/ポート番号:{1}", remoteEP.Address, remoteEP.Port);
-
-                    for(int i = 0; i < 100000; ++i)
-                    {
-                        Console.WriteLine();
-                    }
-
-                    //戻り値をクライアントに戻す
-                    string sendMsg = 1.ToString();
+                    string sendMsg = "test";
                     byte[] buff = Encoding.UTF8.GetBytes(sendMsg);
-                    m_udpClient?.Send(buff, buff.Length, remoteEP.Address.ToString(), remoteEP.Port);
+                    udpClient?.Send(buff, buff.Length, remoteHost, remotePort);
                 }
-            }
-        }
 
-        static UdpClient m_udpClient;
+                //戻り値取得
+                IPEndPoint remoteEP = null;
+                udpClient.Client.ReceiveTimeout = 10000;
+                int rtv = -1;
+                {
+                    byte[] buff = null;
+                    try
+                    {
+                        buff = udpClient?.Receive(ref remoteEP);
+                    }
+                    catch (SocketException e)
+                    {
+                    }
+                    
+                    if (null != buff)
+                    {
+                        string rcvMsg = Encoding.UTF8.GetString(buff);
+                        int.TryParse(rcvMsg, out rtv);
+                    }
+                }
+
+                Console.WriteLine(rtv);
+
+                //UdpClientを閉じる
+                udpClient?.Close();
+            }
+
+
+            Console.ReadLine();
+        }
     }
 }
